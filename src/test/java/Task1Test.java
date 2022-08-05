aa0eec0d950cd5e3e3bcf622a0a1fb278b339984
Matthew Edmundson <edmundmatt@ecs.vuw.ac.nz>
2022-08-05 13:05:00 +1200

    Added mre testing - not all passing.

--- src/test/java/Task1Test.java
+++ src/test/java/Task1Test.java
@@ -1,6 +1,7 @@
 import nz.ac.wgtn.ecs.swen438.assignment1.mayan.Task1;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -30,5 +31,15 @@ public class Task1Test {
         for (String digit: validDigits) {
             assertTrue(Task1.isValidMayanNumberExpression(digit));
         }
+    }
+
+    @Test
+    public void testFail1() {
+        assertFalse(Task1.isValidMayanNumberExpression("*****"));
+    }
+
+    @Test
+    public void testFail2() {
+        assertFalse(Task1.isValidMayanNumberExpression("*0"));
     }
 }
