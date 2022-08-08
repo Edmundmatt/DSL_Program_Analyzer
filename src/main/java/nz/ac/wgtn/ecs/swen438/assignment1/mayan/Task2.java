a8d0358aaac85e16c56a8699cb66a050f5c3a0bf
Matthew Edmundson <edmundmatt@ecs.vuw.ac.nz>
2022-08-08 16:01:42 +1200

    Updated Task2 + Task2Test.

--- src/main/java/nz/ac/wgtn/ecs/swen438/assignment1/mayan/Task2.java
+++ src/main/java/nz/ac/wgtn/ecs/swen438/assignment1/mayan/Task2.java
@@ -1,13 +1,41 @@
 package nz.ac.wgtn.ecs.swen438.assignment1.mayan;
 
 
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.xpath.XPath;
+
+import java.util.Collection;
+
 public class Task2 {
     public static int evaluateMayanNumberExpression(String input) throws Exception {
-        /**
-         * replace this dummy to solve task 1 -- additional helper classes can be used
-         * an exception (any type) should be thrown if the input cannot be parsed
-         * DO NOT CHANGE THE NAME OR SIGNATURE OF THIS CLASS AND METHOD !
-         */
-        return 0;
+        try {
+//            MayanMathLexer lexer = new MayanMathLexer(CharStreams.fromString(input));
+//            CommonTokenStream tokens = new CommonTokenStream(lexer);
+//            MayanMathParser parser = new MayanMathParser(tokens);
+//            XPath p = new XPath(parser, input);
+//            Collection<ParseTree> results = p.evaluate(parser.expression());
+//
+//            for (ParseTree solution : results) {
+//                String parseTreeAsString = solution.toStringTree(parser);
+//                System.out.println(parseTreeAsString + '\n');
+//                System.out.println(solution.getText());
+//                System.out.println();
+
+            MayanMathLexer lexer = new MayanMathLexer(CharStreams.fromString(input));
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            new MayanMathParser(tokens).expression();
+
+            for (Token token:tokens.getTokens()) {
+                System.out.println(token);
+                System.out.println(token.getText());
+            }
+            return 0;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
     }
 }
---
+++ src/test/java/Task2Test.java
@@ -0,0 +1,68 @@
+import nz.ac.wgtn.ecs.swen438.assignment1.mayan.Task2;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Task2Test {
+
+    @Test
+    public void test1() throws Exception {
+        assertEquals(Task2.evaluateMayanNumberExpression("*"), 1);
+    }
+
+    @Test
+    public void test2() throws Exception {
+        assertEquals(Task2.evaluateMayanNumberExpression("0"), 0);
+    }
+
+    @Test
+    public void test3() throws Exception {
+        assertEquals(Task2.evaluateMayanNumberExpression("**"), 2);
+    }
+
+    @Test
+    public void test4() throws Exception {
+        assertEquals(Task2.evaluateMayanNumberExpression("***"), 3);
+    }
+
+    @Test
+    public void test5() throws Exception {
+        assertEquals(Task2.evaluateMayanNumberExpression("|"), 5);
+    }
+
+    @Test
+    public void test6() throws Exception {
+        assertEquals(Task2.evaluateMayanNumberExpression("****|"), 9);
+    }
+
+    @Test
+    public void test7() throws Exception {
+        assertEquals(Task2.evaluateMayanNumberExpression("||"), 10);
+    }
+
+    @Test
+    public void test8() throws Exception {
+        assertEquals(Task2.evaluateMayanNumberExpression("*_*"), 21);
+    }
+
+    @Test
+    public void test9() throws Exception {
+        assertEquals(Task2.evaluateMayanNumberExpression("*_*||"), 31);
+    }
+
+    @Test
+    public void testAddition1() throws Exception {
+        assertEquals(Task2.evaluateMayanNumberExpression("***+*"), 4);
+    }
+
+    @Test
+    public void testAddition2() throws Exception {
+        assertEquals(Task2.evaluateMayanNumberExpression("***+|"), 8);
+    }
+
+    @Test
+    public void testAddition3() throws Exception {
+        assertEquals(Task2.evaluateMayanNumberExpression("|+*"), 6);
+    }
+}
